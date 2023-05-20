13face7cf30e6d42b752a71cf45c763efd9267e3
Fatou Bintou GASSAMA <gassamafatoubintou7@gmail.com>
2023-05-20 18:30:42 +0000

    mis a jour

--- Gestion_reservation/dall_diamm/admin.py
+++ Gestion_reservation/dall_diamm/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from .models import Voiture
+from .models import voiture,chambre
+
+
+class Adminvoiture(admin.ModelAdmin):
+    list_display=('nom_voit','prix')
+    list_filter=['nom_voit']
+
+
+class Adminchambre(admin.ModelAdmin):
+    list_display=('num_chmb','nom_chmb','prix_chmb','date_location')
+    
 
 # Register your models here.
-admin.site.register(Voiture)
+admin.site.register(voiture,Adminvoiture)
+admin.site.register(chambre,Adminchambre)
